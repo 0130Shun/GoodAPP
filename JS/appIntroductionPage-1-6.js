d83b1c638cfeb43190ceca89b66d5c33b3ba9c47
shun <work19950130@gmail.com>
2023-11-30 22:26:19 +0800

    上傳新檔案

---
+++ JS/appIntroductionPage-1-6.js
@@ -0,0 +1,5 @@
+const scoreCommentBtn = document.querySelector('.scoreCommentBtn');
+
+scoreCommentBtn.addEventListener('click',function(e){
+    window.open('commentScorePage-1-6-1.html','width=200','height=200');
+})
