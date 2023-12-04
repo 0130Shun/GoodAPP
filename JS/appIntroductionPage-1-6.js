046b69f857082734033bdf0e94edca04b433f5c6
shun <work19950130@gmail.com>
2023-12-04 22:13:41 +0800

    HTML-新增隱藏段落，CSS-新增隱藏段落樣式及部分RWD，JS加上兩個按鈕的監聽

--- JS/appIntroductionPage-1-6.js
+++ JS/appIntroductionPage-1-6.js
@@ -1,17 +1,33 @@
-const modal = document.querySelector(".modal");
+// 評論/評分按鈕
 const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
+const commentModal = document.querySelector(".commentModal");
+const commentBtnClose = document.querySelector(".commentBtnClose");
 const scoreCommentBtn = document.querySelector(".scoreCommentBtn");
 
 //監聽openModal的按鈕，點擊後，移除兩個標籤上面的class
 scoreCommentBtn.addEventListener('click',function(e){
-    modal.classList.remove("hidden");
+    commentModal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 })
 
 //監聽closeModal的按鈕，點擊後，加入class到兩個標籤上
-closeModalBtn.addEventListener('click',function(e){
-    modal.classList.add("hidden");
+commentBtnClose.addEventListener('click',function(e){
+    commentModal.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
+
+//分享按鈕
+const shareModal = document.querySelector(".shareModal");
+const shareBtnClose = document.querySelector(".shareBtnClose");
+const shareBtn = document.querySelector(".shareBtn");
+
+//監聽shareBtn的按鈕，點擊後，移除兩個標籤上的class
+shareBtn.addEventListener('click',function(e){
+    shareModal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+})
+
+shareBtnClose.addEventListener('click',function(e){
+    shareModal.classList.add("hidden");
     overlay.classList.add("hidden");
 })
