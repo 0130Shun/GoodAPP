11587c731e9aa50c039aaad11621379d96c558d8
shun <work19950130@gmail.com>
2023-12-11 22:44:48 +0800

    新增JS，修改html樣式

--- JS/appIntroductionPage-1-6.js
+++ JS/appIntroductionPage-1-6.js
@@ -4,17 +4,21 @@ const commentModal = document.querySelector(".commentModal");
 const commentBtnClose = document.querySelector(".commentBtnClose");
 const scoreCommentBtn = document.querySelector(".scoreCommentBtn");
 
-//監聽openModal的按鈕，點擊後，移除兩個標籤上面的class
+//開啟評分評論彈跳視窗
 scoreCommentBtn.addEventListener('click',function(e){
     commentModal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 })
 
-//監聽closeModal的按鈕，點擊後，加入class到兩個標籤上
+//關閉評分評論彈跳視窗
 commentBtnClose.addEventListener('click',function(e){
+    commentClose();
+})
+//重構關閉評分評論彈跳視窗
+function commentClose(){
     commentModal.classList.add("hidden");
     overlay.classList.add("hidden");
-})
+}
 
 //分享按鈕
 const shareModal = document.querySelector(".shareModal");
@@ -27,7 +31,23 @@ shareBtn.addEventListener('click',function(e){
     overlay.classList.remove("hidden");
 })
 
+//監聽關閉按鈕，點擊後，加上兩個標籤上的class
 shareBtnClose.addEventListener('click',function(e){
     shareModal.classList.add("hidden");
     overlay.classList.add("hidden");
+})
+
+//監聽評論送出按鈕
+const send = document.querySelector('.send');
+let comment = document.getElementById('comment');
+console.log(comment.value);
+send.addEventListener('click',function(e){
+    if(comment.value === ""){
+        alert('請輸入評論');
+        return;
+    }else{
+        alert('送出成功!!!');
+        comment.value = "";
+    }
+    commentClose();
 })
